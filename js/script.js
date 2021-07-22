ee87ead600fd63a8d67c17d8d032e4a8ffd8ad9d
Valentina-Fr <v_frasca@virgilio.it>
2021-07-22 14:30:14 +0200

    Complete const card

--- js/script.js
+++ js/script.js
@@ -5,4 +5,42 @@ const card = {
     combinedManaCost: 8,
     cardType: "Creature",
     subType: "Giant",
-}
+    expansion: {
+        name: "9th Expansion",
+        reissueId: 9,
+        rarity: "Golden",
+        collectionNr: 4,
+        totalCards: 42
+    },
+    flavorText: {
+        quote: "Occorse tutta la sua forza per contenere le fiamme che ardevano dentro di lui.",
+        author: "Greg Staples"
+    },
+    abilities: [
+        {
+            launchCost: ["R", "B"],
+            description: "Il Colosso Sangue di Fuoco infligge 6 danni a ogni creatura e a ogni giocatore."
+        },
+        {
+            launchCost: ["W", "B"],
+            description: "Bloodfire Colossus deals 6 damage to each creature and each player."
+        }
+    ],
+    illustrattion: {
+        author: {
+        id: 1,
+        name: "John Smith" 
+        },
+        source: "img/picture.png"
+    },
+    collectionNr: "14/23",
+    constitution: 6,
+    strength: 6,
+    borderColor: "#000",
+    background: {
+        color: "red",
+        source: "img/image.png"
+    }
+};
+
+console.log(card);
